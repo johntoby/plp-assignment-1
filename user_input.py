d2f1b881747ae267d4d8b189aa2fbd36a4c7aec1
John Idowu <johntoby007@gmail.com>
2024-05-26 08:26:45 +0100

    Added source code

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# program to accept user details via input 
+
+name = input("Enter your name: ")
+age = input("Enter your age: ") 
+location = input("Enter your location: ")
+print ('Welcome ' + name + '! Your age is ' + age + ' and you are from ' + location) 
